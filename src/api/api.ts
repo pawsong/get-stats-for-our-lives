9c2c9bb565c4a8c446129f8e98fb2617e56406a1
Stuart Schechter <UppaJung@users.noreply.github.com>
2018-03-12 06:30:13 +0900

    Pass errors across domains for debugging if problems occur (#8)

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ import {StatsForOurLives} from "../server/stats-for-our-lives"
 import {MarchForOurLivesEvent} from "../server/event";
 type Handler<RESPONSE> = (response: RESPONSE) => any;
 
-const handlers = new Map<string, Handler<any>>();
+const handlers = new Map<string, {success: Handler<any>, failure: Handler<any>}>();
 let uniqueRequenstIndex = 0;
 let uniquishString =
   "And the NRA would have gotten away with it too if it hadn't been for those meddling youngsters.";
@@ -18,14 +18,19 @@ document.body.appendChild(iframe);
 const origin = `http:///`;
 
 function onMessage(this: Window, event: WindowEventMap["message"]) {
-  const {data} = event;
-  if ("type" in data && "uniqueRequestId" in data && "payload" in data) {
-    const message = data as CrossDomainMessage & {payload: any};
-    if (handlers.has(message.uniqueRequestId)) {
-      const handler = handlers.get(message.uniqueRequestId);
-      if (handler) {
-        handler(message.payload);
-      }
+  const data: CrossDomainAPIs.Responses = event.data;
+  if ("name" in data && "uniqueRequestId" in data && handlers.has(data.uniqueRequestId)) {
+    const {success, failure} = handlers.get(data.uniqueRequestId) || {success: ()=>undefined, failure: () => undefined};
+    // This appears to be a correctly formatted CrossDomainAPI response
+    if ("payload" in data) {
+      // Success case where payload is returned
+      const message = data as CrossDomainMessage & {payload: any};
+      success(message.payload);
+    } else {
+      const error = data.error;
+      const e = new Error(data.error.body);
+      Object.assign(e, {statusCode: error.code});
+      failure(e)
     }
   }
 }
@@ -34,17 +39,17 @@ window.addEventListener("message", onMessage);
 
 function sendRequestFactory<API extends CrossDomainAPIs.WithoutPayload>(
   name: API["request"]["name"]
-): () => Promise<API["response"]["payload"]>;
+): () => Promise<CrossDomainAPIs.ResponsePayload<API>>;
 function sendRequestFactory<API extends CrossDomainAPIs.WithPayload>(
   name: API["request"]["name"]
-): (payload: API["request"]["payload"]) => Promise<API["response"]["payload"]>;
+): (payload: API["request"]["payload"]) => Promise<CrossDomainAPIs.ResponsePayload<API>>;
 function sendRequestFactory<API extends CrossDomainAPIs>(name: API["request"]["name"]):
-  (payload: API["request"]["payload"]) => Promise<API["response"]["payload"]>
+  (payload: API["request"]["payload"]) => Promise<CrossDomainAPIs.ResponsePayload<API>>
  {
-    return (payload: API["request"]["payload"]): Promise<API["response"]["payload"]> => {
+    return (payload: API["request"]["payload"]): Promise<CrossDomainAPIs.ResponsePayload<API>> => {
       const uniqueRequestId = uniquishString + (uniqueRequenstIndex++).toString();
-      const promise = new Promise<API["response"]["payload"]>( (resolve ) => {
-        handlers.set(uniqueRequestId, resolve);
+      const promise = new Promise<CrossDomainAPIs.ResponsePayload<API>>( (resolve, reject) => {
+        handlers.set(uniqueRequestId, {success: resolve, failure: reject});
       });
       window.postMessage({
         name,
